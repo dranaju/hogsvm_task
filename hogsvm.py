b4ef48f3f6887c8d20072a18d1bf8ccf376947ce
Dranaju <dranaju@gmail.com>
2024-11-29 17:56:45 -0300

    tqdm integration

--- hogsvm.py
+++ hogsvm.py
@@ -8,6 +8,7 @@ from skimage.feature import hog
 from sklearn import svm
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score
+from tqdm import tqdm
 
 train_data_dir = '../archive/dogvscat_small/train'
 
@@ -25,7 +26,7 @@ def load_data(data_dir, img_width=84, img_height=84):
         label = class_index
         class_dir = os.path.join(data_dir, class_name)
 
-        for image_name in os.listdir(class_dir):
+        for image_name in tqdm(os.listdir(class_dir)):
             image_path = os.path.join(class_dir, image_name)
             image = Image.open(image_path).convert('L')
             image = image.resize((img_width, img_height))
@@ -46,7 +47,7 @@ X_train_hog = []
 X_train_hog_features = []
 X_test_hog = []
 X_test_hog_features = []
-for image_train in X_train:
+for image_train in tqdm(X_train):
     fd, hog_image = hog(image_train, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True)
     X_train_hog.append(hog_image)
     X_train_hog_features.append(fd)
@@ -70,11 +71,20 @@ grid = GridSearchCV(clf, params, refit=True, verbose=3, cv=4)
 grid.fit(X_train_hog_features, y_train)
 
 Y_pred_train = grid.predict(X_train_hog_features)
-accuracy = accuracy_score(y_train, Y_pred_train)
-print(f'Accuracy train: {100*accuracy:.2f}')
+accuracy_train = accuracy_score(y_train, Y_pred_train)
+print(f'Accuracy train: {100*accuracy_train:.2f}')
 
 Y_pred = grid.predict(X_test_hog_features)
-accuracy = accuracy_score(y_test, Y_pred)
-print(f'Accuracy test: {100*accuracy:.2f}')
+accuracy_test = accuracy_score(y_test, Y_pred)
+print(f'Accuracy test: {100*accuracy_test:.2f}')
 
 print(grid.best_params_)
+
+#save accuracy and best params
+with open('hogsvm.txt', 'a') as f:
+    f.write(f'Accuracy train: {100*accuracy_train:.2f}\n')
+    f.write(f'Accuracy test: {100*accuracy_test:.2f}\n')
+    f.write(f'Best params: {grid.best_params_}\n')
+    f.write(f'------\n')
+    f.close()
+
